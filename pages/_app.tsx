545ccd111b093f328acff9e7a480522708d6df10
jopijuco <jpjc@protonmail.com>
2022-08-12 00:44:42 +0100

    Revert default styles

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
 import '../styles/globals.css'
-import '../styles/header.scss'
 import type { AppProps } from 'next/app'
+import '../styles/Home.module.css'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
