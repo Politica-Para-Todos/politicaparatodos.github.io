ecbf6c36683366fef4114d182e0b3a1ec63b0e77
jopijuco <jpjc@protonmail.com>
2022-08-24 23:49:12 +0100

    Validation to avoid hydrationg error

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,10 @@ import '../styles/index.scss';
 import type { AppProps } from 'next/app';
 
 const MyApp = ({ Component, pageProps}: AppProps) => {
+
+  if (typeof window === 'undefined') {
+    return <></>
+  }
   return <Component {...pageProps} />
 }
 
