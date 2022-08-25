117433af90a545827e69d2007a7ce0a98ca3bf71
jopijuco <jpjc@protonmail.com>
2022-08-25 17:01:38 +0100

    WIP - Fix linting

--- src/components/layout-footer.tsx
+++ src/components/layout-footer.tsx
@@ -5,6 +5,7 @@ import socialSharing from "./social-sharing";
 import VerticalLogo from '../../public/vertical_logo.jpg'
 import ContributorsList from "./contributors";
 import { memo } from "react";
+import Link from "next/link";
 
 const HomeFooter = () => {
 
@@ -19,7 +20,11 @@ const HomeFooter = () => {
         </Col>
         <Col span={24} md={{ offset: 4, span: 16 }} lg={{ offset: 0, span: 12 }} className="footer-info">
           <h2>Quem Somos</h2>
-          <p>Podes consultar <a rel="noopener" href="/about-us">aqui</a> a equipa por detrás do projecto e as suas motivações.</p>
+          <p>Podes consultar
+            <Link rel="noopener" href="/about-us">
+              <a>aqui</a>
+            </Link> a equipa por detrás do projecto e as suas motivações.</p>
+          {/* <p>Podes consultar <a rel="noopener" href="/about-us">aqui</a> a equipa por detrás do projecto e as suas motivações.</p> */}
           <br />
           <ContributorsList />
         </Col>
