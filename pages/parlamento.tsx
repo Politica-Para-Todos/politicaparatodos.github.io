63ae7cd7523ae6472a6fce7a2107c5e42e654a76
jopijuco <jpjc@protonmail.com>
2022-08-27 00:32:05 +0100

    Restructure parlament page

--- pages/parlamento.tsx
+++ pages/parlamento.tsx
@@ -1,40 +1,17 @@
-import { ObjectMapper } from "jackson-js";
-import { GetStaticProps, NextPage } from "next";
-import { Legislature, DeputyDetails } from "../src/dtos/base-info";
+import { NextPage } from "next";
+import { Layout } from 'antd';
+import LayoutHeader from "../src/components/layout-header";
 
-const Parlament: NextPage = () => {
-  
-  return (
-    <div>LOL</div>
-  )
+const Parlament: NextPage = () => (
+  <Layout>
+    <LayoutHeader />
+    <Layout.Content>
+      <h2>Parlament</h2>
+    </Layout.Content>
+  </Layout>
+)
   // const mapper = new ObjectMapper();
   // const legislature: Legislature = mapper.parse<Legislature>(JSON.stringify(data.legislature), { mainCreator: () => [Legislature] })
-
-  // return (
-  //   <div>
-  //     <div>DEPUTIES - PARTIES</div>
-  //     <ul>
-  //       {
-  //         legislature.deputy.details.map((dep: DeputyDetails) => (
-  //           // eslint-disable-next-line react/jsx-key
-  //           <li accessKey={dep.id.toString()}>{dep.fullName} - {dep.position?.details.description}</li>
-  //         ))
-  //       }
-  //     </ul>
-  //     <div>HEMICICLO</div>
-  //     <div>
-  //       <ul>
-  //         {
-  //           legislature.parlamentaryGroup.details.map((party: any) => (
-  //             // eslint-disable-next-line react/jsx-key
-  //             <li accessKey={party.acronym}>{party.name}</li>
-  //           ))
-  //         }
-  //       </ul>
-  //     </div>
-  //   </div>
-  // )
-}
 
 // export const getStaticProps: GetStaticProps = async () => {
 //   const baseInfoRequest = fetch('https://app.parlamento.pt/webutils/docs/doc.txt?path=6148523063446f764c324679626d56304c3239775a57356b595852684c3052685a47397a51574a6c636e52766379394a626d5a76636d3168773666446f32386c4d6a424359584e6c4c3168574a5449775447566e61584e7359585231636d45765357356d62334a7459574e6862304a6863325659566c3971633239754c6e523464413d3d&fich=InformacaoBaseXV_json.txt&Inline=true')
@@ -64,4 +41,4 @@ const Parlament: NextPage = () => {
 //   }
 // }
 
-export default Parlament
+export default Parlament;
