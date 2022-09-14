495247249f584661bb1e2077c840297fc59f4d43
jopijuco <jpjc@protonmail.com>
2022-09-14 18:22:24 +0100

    Fix requests, add interventions

--- src/dtos/deputy-activities/requests.ts
+++ src/dtos/deputy-activities/requests.ts
@@ -1,9 +1,14 @@
-import { JsonClassType, JsonIgnoreProperties, JsonManagedReference, JsonProperty } from "jackson-js";
+import { JsonClassType, JsonDeserialize, JsonFormat, JsonFormatShape, JsonIgnoreProperties, JsonManagedReference, JsonProperty } from "jackson-js";
+import { buildArray } from "../../utils/parlamento-api";
 
 export class RequestActivity {
 
   @JsonProperty({ value: 'pt_gov_ar_wsar_objectos_RequerimentosOut' })
   @JsonManagedReference() @JsonClassType({ type: () => [Array, [RequestDetails]] })
+  @JsonFormat({ shape: JsonFormatShape.ARRAY })
+  @JsonDeserialize({
+    using: buildArray
+  })
   requestDetails: RequestDetails[];
 
   constructor(requestDetails: RequestDetails[]) {
@@ -56,63 +61,5 @@ export class RequestDetails {
     this.subject = subject;
     this.date = date;
     this.perTp = perTp;
-  }
-}
-
-@JsonIgnoreProperties({ value: ['pubLg'] })
-export class InterventionDetails {
-
-  @JsonProperty({ value: 'intId' })
-  id: number;
-
-  @JsonProperty({ value: 'intTe' })
-  intTe: string;
-
-  @JsonProperty({ value: 'intSu' })
-  intSu: string;
-
-  @JsonProperty({ value: 'pubDtreu' })
-  pubDtreu: string;
-
-  @JsonProperty({ value: 'pubTp' })
-  pubTp: string;
-
-  @JsonProperty({ value: 'pubLg' })
-  legislature: string;
-
-  @JsonProperty({ value: 'pubSl' })
-  pubSl: string
-
-  @JsonProperty({ value: 'pubNr' })
-  number: number
-
-  @JsonProperty({ value: 'tinDs' })
-  tinDs: string
-
-  @JsonProperty({ value: 'pubDar' })
-  pubDar: string
-
-  constructor(
-    id: number,
-    intTe: string,
-    intSu: string,
-    pubDtreu: string,
-    pubTp: string,
-    legislature: string,
-    pubSl: string,
-    number: number,
-    tinDs: string,
-    pubDar: string
-  ) {
-    this.id = id;
-    this.intTe = intTe;
-    this.intSu = intSu;
-    this.pubDtreu = pubDtreu;
-    this.pubTp = pubTp;
-    this.legislature = legislature;
-    this.pubSl = pubSl;
-    this.number = number;
-    this.tinDs = tinDs;
-    this.pubDar = pubDar;
   }
 }
