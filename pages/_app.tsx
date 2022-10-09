c304cb10c45f0bd82fff5dfa62bd6d16e02cdeda
jopijuco <jpjc@protonmail.com>
2022-10-09 01:29:57 +0100

    Config changes

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
 import '../styles/index.scss';
 import type { AppProps } from 'next/app';
 import { ApolloProvider } from '@apollo/client';
-import apolloClient from '../src/graphql/apollo-service';
+import apolloClient from '../src/graphql/apollo-client';
 
 const App = ({ Component, pageProps }: AppProps) =>
   <ApolloProvider client={apolloClient}>
