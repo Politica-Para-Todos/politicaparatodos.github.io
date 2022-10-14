a95e3194607f88da6651f4b9277c0f79e1f90037
jopijuco <jpjc@protonmail.com>
2022-10-14 19:33:13 +0100

    Fix todo

--- src/graphql/apollo-client.ts
+++ src/graphql/apollo-client.ts
@@ -8,5 +8,5 @@ const apolloClient = new ApolloClient({
   cache: new InMemoryCache()
 });
 
-// TODO - Abstract this to a service to be initialized on nextjs startup and exported to be used whenever needed
+// TODO - Abstract to a service on bootstrap and exported to be used whenever needed
 export default apolloClient;
