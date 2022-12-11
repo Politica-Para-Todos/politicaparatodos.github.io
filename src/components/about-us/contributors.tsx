cc143d5395be38e1dd3758e50f769c0cf2339688
João <joaovlsaro@gmail.com>
2022-12-11 20:26:37 +0000

    Fix hydration issue

--- src/components/about-us/contributors.tsx
+++ src/components/about-us/contributors.tsx
@@ -1,19 +1,42 @@
 import React from "react";
 import { Row, Col } from "antd";
-import { shuffleList, contributors } from "../../utils/contributors";
+import ContributorsList from "../contributors-list";
 
-const AboutUsContributors = () => (
-  <section className="about-us-contributors">
-    <Row typeof="flex" justify="space-between" align="middle">
-      <Col span={24}>
-        <h2 className="about-us-contributors-title">Quem Somos</h2>
-        <p>A comunidade Política Para Todos nasceu no verão de 2019 com o objetivo de promover a participação ativa dos cidadãos nos processos eleitorais em Portugal. É composta por voluntários de várias partes do país e diferentes áreas profissionais. Adotamos princípios de transparência radical na nossa forma de trabalho: toda a comunicação interna está disponível numa plataforma aberta, as reuniões são gravadas e também de acesso livre. A adesão à comunidade é aberta para qualquer cidadão.</p>
-        <br />
-        <p>Podes consultar <a target="_blank" rel="noopener noreferrer" href="https://www.loomio.org/g/ZqT2uPv6/politica-para-todos">aqui</a> todo o histórico do projecto.</p>
-        <p className="about-us-contributors-list">{shuffleList(contributors).join(", ")}.</p>
-      </Col>
-    </Row>
-  </section >
-);
+const AboutUsContributors = () => {
+  return (
+    <section className="about-us-contributors">
+      <Row typeof="flex" justify="space-between" align="middle">
+        <Col span={24}>
+          <h2 className="about-us-contributors-title">Quem Somos</h2>
+          <p>
+            A comunidade Política Para Todos nasceu no verão de 2019 com o
+            objetivo de promover a participação ativa dos cidadãos nos processos
+            eleitorais em Portugal. É composta por voluntários de várias partes
+            do país e diferentes áreas profissionais. Adotamos princípios de
+            transparência radical na nossa forma de trabalho: toda a comunicação
+            interna está disponível numa plataforma aberta, as reuniões são
+            gravadas e também de acesso livre. A adesão à comunidade é aberta
+            para qualquer cidadão.
+          </p>
+          <br />
+          <p>
+            Podes consultar{" "}
+            <a
+              target="_blank"
+              rel="noopener noreferrer"
+              href="https://www.loomio.org/g/ZqT2uPv6/politica-para-todos"
+            >
+              aqui
+            </a>{" "}
+            todo o histórico do projecto.
+          </p>
+          <p className="about-us-contributors-list">
+            <ContributorsList />
+          </p>
+        </Col>
+      </Row>
+    </section>
+  );
+};
 
 export default AboutUsContributors;
