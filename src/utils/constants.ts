58fd7c8e7205b0f692c299c24f3ba827a1941fbb
jopijuco <jpjc@protonmail.com>
2022-12-13 00:40:55 +0000

    Add constants file

---
+++ src/utils/constants.ts
@@ -0,0 +1,16 @@
+// URL
+export const PORTAL_ELEITOR_URL = "https://www.portaldoeleitor.pt/Default.aspx";
+export const LOOMIO_PPT_URL = "https://www.loomio.org/g/ZqT2uPv6/politica-para-todos"
+export const PPT_WEBSITE = "https://politicaparatodos.pt/"
+export const TWITTER_PPT = "https://twitter.com/politicaparatds"
+export const FACEBOOK_PPT = "http://fb.me/politicaparatodos.pt"
+export const MEDIUM_PPT = "https://medium.com/politica-para-todos"
+export const INSTAGRAM_PPT = "https://www.instagram.com/politica_para_todos/"
+export const GITHUB_PPT = "https://github.com/Politica-Para-Todos/"
+export const YOUTUBE_VIDEO_EP1 = "https://www.youtube.com/watch?v=up0Gfd5c0cM"
+export const YOUTUBE_VIDEO_EP2 = "https://www.youtube.com/watch?v=2nBGppKe1z4"
+export const YOUTUBE_VIDEO_EP3 = "https://www.youtube.com/watch?v=OERxKenLIo8"
+export const YOUTUBE_VIDEO_EP4 = "https://www.youtube.com/watch?v=v-Uj7kwfG5o"
+
+// Email
+export const EMAIL_PPT = "contacto@politicaparatodos.pt"
