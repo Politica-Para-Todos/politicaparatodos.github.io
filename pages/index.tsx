3631d3e0aa96de5ab14900af8f9fc9cddbba776e
João Saro <joao.saro@pipedrive.com>
2023-05-14 17:06:28 +0100

    Homepage parties fetch

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,44 +5,68 @@ import LayoutHeader from "../components/global/layout-header";
 import MetaTags from "../components/global/meta-tags";
 import HomeCountdown from "../components/home/countdown";
 import HomeMedia from "../components/home/media";
-import { HomeMission, HomeMissionInfographic } from "../components/home/mission";
+import {
+  HomeMission,
+  HomeMissionInfographic,
+} from "../components/home/mission";
 import HomeParties from "../components/home/parties";
 import { HomePageParty } from "../src/retriever/dtos/party-dto";
-import { Retriever, SeedsJsonRetriever } from "../src/retriever/service";
 
 interface HomePageProps {
-  homePageParties: HomePageParty[];
+  parties: HomePageParty[];
 }
 
-const Home: NextPage<HomePageProps> = ({ homePageParties }) =>
-  <Layout>
-    <MetaTags
-      pageTitle="Política Para Todos"
-      pageDescription="A comunidade Política Para Todos nasceu no verão de 2019 com o objetivo de promover a participação ativa dos cidadãos nos processos eleitorais em Portugal. É composta por voluntários de várias partes do país e diferentes áreas profissionais."
-      socialTitle="Quem somos"
-      socialDescription="A comunidade Política Para Todos nasceu no verão de 2019 com o objetivo de promover a participação ativa dos cidadãos nos processos eleitorais em Portugal. É composta por voluntários de várias partes do país e diferentes áreas profissionais."
-      socialImage="/vertical_logo.jpg"
-    />
-    <LayoutHeader />
-    <Layout.Content>
-      <HomeMissionInfographic />
-      <HomeCountdown />
-      <HomeMission />
-      <HomeMedia />
-      <HomeParties parties={homePageParties} />
-      <div className="getsocial gs-inline-group"></div>
-    </Layout.Content>
-    <LayoutFooter />
-  </Layout>
+const Home: NextPage<HomePageProps> = ({ parties }) => {
+  return (
+    <Layout>
+      <MetaTags
+        pageTitle="Política Para Todos"
+        pageDescription="A comunidade Política Para Todos nasceu no verão de 2019 com o objetivo de promover a participação ativa dos cidadãos nos processos eleitorais em Portugal. É composta por voluntários de várias partes do país e diferentes áreas profissionais."
+        socialTitle="Quem somos"
+        socialDescription="A comunidade Política Para Todos nasceu no verão de 2019 com o objetivo de promover a participação ativa dos cidadãos nos processos eleitorais em Portugal. É composta por voluntários de várias partes do país e diferentes áreas profissionais."
+        socialImage="/vertical_logo.jpg"
+      />
+      <LayoutHeader />
+      <Layout.Content>
+        <HomeMissionInfographic />
+        <HomeCountdown />
+        <HomeMission />
+        <HomeMedia />
+        <HomeParties parties={parties} />
+        <div className="getsocial gs-inline-group"></div>
+      </Layout.Content>
+      <LayoutFooter />
+    </Layout>
+  );
+};
 
 export const getStaticProps = async () => {
-  const retriever: SeedsJsonRetriever = new Retriever();
+  return fetch("https://www.politicaparatodos.pt/parties.json")
+    .then((res) => res.json())
+    .then((data) => {
+      let parties = data.map((p: any) => {
+        return {
+          logoFileName: p.logo,
+          acronym: p.acronym,
+          name: p.title,
+        };
+      });
 
-  return {
-    props: {
-      homePageParties: retriever.homePageParties()
-    },
-  };
+      return {
+        props: {
+          parties,
+        },
+      };
+    })
+    .catch((error) => {
+      console.log("ppt-ui error", error);
+
+      return {
+        props: {
+          parties: [],
+        },
+      };
+    });
 };
 
 export default Home;
