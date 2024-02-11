4ae59a174fa511db64034eeaf63f7eb699c269a4
Renata Henriques <36205288+renata-henriques@users.noreply.github.com>
2024-02-11 10:49:17 +0100

    New update Debates page.tsx
    Changed 2022 for 2024
    Updated RTP urls 
    Deleted the Google calendar for now

--- pages/debates-2022.tsx
+++ pages/debates-2022.tsx
@@ -8,9 +8,9 @@ import MetaTags from "../components/global/meta-tags";
 const PoliticalDebates: NextPage = () =>
   <Layout>
     <MetaTags
-      pageTitle="Debates Legislativas 2022"
+      pageTitle="Debates Legislativas 2024"
       pageDescription="A comunidade Política Para Todos nasceu no verão de 2019 com o objetivo de promover a participação ativa dos cidadãos nos processos eleitorais em Portugal. É composta por voluntários de várias partes do país e diferentes áreas profissionais."
-      socialTitle="Debates Legilativas 2022"
+      socialTitle="Debates Legilativas 2024"
       socialDescription="A comunidade Política Para Todos nasceu no verão de 2019 com o objetivo de promover a participação ativa dos cidadãos nos processos eleitorais em Portugal. É composta por voluntários de várias partes do país e diferentes áreas profissionais."
       socialImage="/horizontal_logo.svg"
     />
@@ -30,43 +30,29 @@ const PoliticalDebates: NextPage = () =>
           <Col span={24} lg={24} className="home-our-mission-text">
             <h1>Debates televisivos</h1>
             <p>Participam nos debates televisivos todos os partidos que, nas últimas eleições Legislativas (2019), conquistaram lugares parlamentares em várias rondas frente a frente. No final, ainda serão realizados dois debates com todos os partidos eleitos em 2019 (televisão e rádio) e outro com partidos não eleitos há dois anos.</p>
-            <p>Subscreve o nosso calendário com horários dos debates actualizados:
-              <a
-                href="https://bit.ly/debates22-PPT"
-                target="_blank"
-                rel="noopener noreferrer"> Calendário Google de debates 2022 <LinkOutlined />
-              </a>
-            </p>
             <Table dataSource={debateDates} columns={tableColumns} className="debates__table" />
             <Card title="Perdeste um debate?">
               <p>A RTP Play disponibiliza todos os debates independentemente do canal que tenha exibido. Aproveita para rever o debate que procuras:</p>
               <ul>
                 <li>
                   <a
-                    href="https://www.rtp.pt/play/p9711/legislativas-2022-debates-rtp"
+                    href="https://www.rtp.pt/play/p12900/debates-legislativas-2024"
                     target="_blank"
                     rel="noopener noreferrer">Debates RTP/RTP3 <LinkOutlined />
                   </a>
                 </li>
                 <li>
                   <a
-                    href="https://www.rtp.pt/play/p9787/legislativas-2022-debates-sic-sic-noticias"
+                    href="https://www.rtp.pt/play/p12899/debates-legislativas-2024-sicsic-noticias"
                     target="_blank"
                     rel="noopener noreferrer">Debates SIC/SIC Notícias <LinkOutlined />
                   </a>
                 </li>
                 <li>
                   <a
-                    href="https://www.rtp.pt/play/p9788/legislativas-2022-debates-tvi-cnn"
+                    href="https://www.rtp.pt/play/p12901/debates-legislativas-2024-tvicnn"
                     target="_blank"
                     rel="noopener noreferrer">Debates TVI/CNN Portugal <LinkOutlined />
-                  </a>
-                </li>
-                <li>
-                  <a
-                    href="https://www.rtp.pt/play/p9596/eleicoes-legislativas-2022-entrevistas-lideres-partidarios"
-                    target="_blank"
-                    rel="noopener noreferrer">Entrevista a lideres partidários <LinkOutlined />
                   </a>
                 </li>
               </ul>
